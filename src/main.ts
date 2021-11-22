201be36f502b7f5401c81b1eea82e828ccbc5612
corporal <corporal9736@gmail.com>
2021-11-22 18:30:03 +0800

    修改readme,并添加axios组件

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router/index'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 
-createApp(App)
-.use(router)
-.mount('#app')
+const app = createApp(App)
+app.use(router)
+app.use(VueAxios, axios)
+app.provide('axios', app.config.globalProperties.axios)
+app.mount('#app')
--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,8 +1,10 @@
 <template>
     <h1>welcome to {{message}}</h1>
-    <router-link to="/about">go to about</router-link>
+    <router-link to="/about">go to about</router-link><br>
+
 </template>
 
 <script setup lang="ts">
     const message = "the home page"
+    
 </script>
