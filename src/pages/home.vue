56472a93694f1a77a1da069adc4e24c97dc18e16
nieyuzhou <rucnyz@gmail.com>
2021-12-23 00:28:50 +0800

    其余界面内容及bug修改完成

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -221,16 +221,16 @@ export default defineComponent({
     window.onscroll = function ()
     {
       //变量scrollTop是滚动条滚动时，距离顶部的距离
-      var scrollTop =
+      const scrollTop =
           document.documentElement.scrollTop || document.body.scrollTop;
       //变量windowHeight是可视区的高度
-      var windowHeight =
+      const windowHeight =
           document.documentElement.clientHeight || document.body.clientHeight;
       //变量scrollHeight是滚动条的总高度
-      var scrollHeight =
+      const scrollHeight =
           document.documentElement.scrollHeight || document.body.scrollHeight;
       //滚动条到底部的条件
-      if (Math.round(scrollTop) + windowHeight == scrollHeight)
+      if (Math.round(scrollTop) + windowHeight === scrollHeight)
       {
         //写后台加载数据的函数
         console.log("到顶部", total.value, goodList.value.length);
